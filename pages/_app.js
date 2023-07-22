b8d8a9ef90b6081723837a0429061ea031a074ab
kranthi kumar <2019271@iiitdmj.ac.in>
2023-07-22 20:23:24 +0530

    name update

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Real Estate</title>
+        <title>Real State</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
